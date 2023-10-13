48902a1f13c544018eafd4e117bd3cc18ff05474
adnanc765 <xkilo1adnan@gmail.com>
2023-10-13 10:55:21 +0400

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,13 @@
+name= "\tAdnan Abbas\n"
+
+print ("unmodified:")
+print (name)
+
+print ("\nUsing lstrip():")
+print (name.lstrip())
+
+print ("\nUsing Rstrip():")
+print (name.rstrip())
+
+print ("\nUsing strip():")
+print (name.strip())
