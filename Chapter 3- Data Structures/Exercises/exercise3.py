d62aac089a48d1afe3cec41b1116717b06e2fe54
adnanc765 <xkilo1adnan@gmail.com>
2023-10-22 23:28:42 +0400

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,8 @@
+transport= " I would like to own a mercedes C class"
+print (transport)
+
+transport=" I would like to own a lexus LS430"
+print (transport)
+
+transport= " I would like to own a bugatti"
+print (transport)
