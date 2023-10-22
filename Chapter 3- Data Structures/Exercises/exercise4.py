27014d404a996661fe94cc2188255162c970e19a
adnanc765 <xkilo1adnan@gmail.com>
2023-10-22 23:29:16 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,9 @@
+guest= ' Cristiano ronaldo' , ' Lionel messi' , ' Diego maradona'
+name= guest[0].title()
+print (name + ", please come to dinner.")
+
+name= guest[1].title()
+print (name + ", please come to dinner.")
+
+name= guest[2].title()
+print (name + ", please come to dinner.")
