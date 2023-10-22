6891b1cba63a80e617850d4b90b2c92c6713e590
adnanc765 <xkilo1adnan@gmail.com>
2023-10-22 23:28:22 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,9 @@
+names =['sultan', 'rauf', 'fahad']
+message= "whats good, " + names[0].title() +"?"
+print (message)
+
+message= "whats good, " + names[1].title() +"?"
+print (message)
+
+message= "whats good, " + names[2].title() +"?"
+print (message)
