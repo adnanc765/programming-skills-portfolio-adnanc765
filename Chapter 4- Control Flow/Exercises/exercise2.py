99ca7089164515452ebfb11f43e8a6f44dbac1ab
adnanc765 <xkilo1adnan@gmail.com>
2023-11-07 18:29:34 +0400

    exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,14 @@
+#if block runs
+alien_color='green'
+
+if alien_color =='green':
+    print(" You just earned 5 points. ")
+else:
+    print(" You just earned 10 points. ")
+#else block runs
+alien_color='yellow'
+
+if alien_color =='green':
+    print(" You just earned 5 points. ")
+else:
+    print(" You just earned 10 points. ")
