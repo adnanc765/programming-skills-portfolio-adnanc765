2b9b0b64dd95fb8795c950cc7e913c12be8f67a1
adnanc765 <xkilo1adnan@gmail.com>
2023-11-07 18:29:21 +0400

    exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,13 @@
+Alien_color=['green']
+if 'green' in Alien_color:
+ print(" you just earned 5 points. ")
+# write a program of this program that passes the if test and another that fails.(fails should have no output.)
+alien_color=['red']
+
+if 'red' in alien_color:
+ print(" you just earned 25 points. ")
+
+if 'yellow' in alien_color:
+ print(" you just earned 50 points. ")
+
+ 
