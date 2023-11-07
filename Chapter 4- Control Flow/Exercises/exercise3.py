e69aeb63b097a79120ac31b06bb6d59b5f8f61c3
adnanc765 <xkilo1adnan@gmail.com>
2023-11-07 18:29:51 +0400

    exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,8 @@
+alien_color='yellow'
+
+if alien_color=='red':
+ print(" You just earned 20 points.")
+if alien_color=='green':
+ print(" You just earned 10 points.")
+else:
+ print(" You just earned 25 points.")
