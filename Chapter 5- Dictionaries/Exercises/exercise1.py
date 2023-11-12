9619774a42cdc15bd5c0b10e936eada2d2053749
adnanc765 <xkilo1adnan@gmail.com>
2023-11-12 21:48:25 +0400

    exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'fahad',
+    'last_name': 'nishan',
+    'age': 19,
+    'city': 'ras al khaimah',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
