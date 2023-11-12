8a5f5a0000ad7f0f85311168aa4294c63103ef7e
adnanc765 <xkilo1adnan@gmail.com>
2023-11-12 22:00:28 +0400

    exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,6 @@
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning the basic of coding, and how to store codes in functions."
+    print(msg)
+
+display_message()
