e2bb5842c78cd6a155471ab8ea22c9ef1a38b8ef
adnanc765 <xkilo1adnan@gmail.com>
2023-11-12 22:00:50 +0400

    exercise3.py

---
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt('extra large', 'I am enevitable')
+make_shirt(message="you are useless.", size='small')
